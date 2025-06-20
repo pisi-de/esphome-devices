bc934db82ce88b97582922a84fbde1d1276e31bc
Christoph Müller <pisi@pisi.de>
2025-06-20 09:48:59 +0200

    add update script

---
+++ update_all.bat
@@ -0,0 +1,33 @@
+echo ===%~f0===
+
+cd /D %~dp0
+
+esphome --version
+
+
+esphome run bme280_bad.yaml --no-logs
+IF %ERRORLEVEL% NEQ 0 (
+  pause something went wrong with bme280_bad
+)
+esphome run bme280_dach.yaml --no-logs
+IF %ERRORLEVEL% NEQ 0 (
+  pause something went wrong with bme280_dach
+)
+esphome run bme280_keller.yaml --no-logs
+IF %ERRORLEVEL% NEQ 0 (
+  pause something went wrong with bme280_keller
+)
+esphome run bme280_kueche.yaml --no-logs
+IF %ERRORLEVEL% NEQ 0 (
+  pause something went wrong with bme280_kueche
+)
+esphome run led-strip-v2.yaml --no-logs
+IF %ERRORLEVEL% NEQ 0 (
+  pause something went wrong with led-strip-v2
+)
+esphome run heizung.yaml --no-logs
+IF %ERRORLEVEL% NEQ 0 (
+  pause something went wrong with heizung
+)
+
+pause all done
